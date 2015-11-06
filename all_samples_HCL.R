a64e0869ca2f1d5aad881578d1877b5eaf1988b9
Dan Schlauch <dschlauch@gmail.com>
2015-11-06 18:37:26 -0500

    more HCL plot stuff

--- all_samples_HCL.R
+++ all_samples_HCL.R
@@ -1,4 +1,19 @@
+library(gplots)
 colorCodes <- c(AFR="red", AMR="green", EUR="blue", SAS="yellow", EAS="black")
+colorCodesPop <- c("dodgerblue2","#E31A1C", # red
+                          "green4",
+                          "#6A3D9A", # purple
+                          "#FF7F00", # orange
+                          "black","gold1",
+                          "skyblue2","#FB9A99", # lt pink
+                          "palegreen2",
+                          "#CAB2D6", # lt purple
+                          "#FDBF6F", # lt orange
+                          "gray70", "khaki2",
+                          "maroon","orchid1","deeppink1","blue1","steelblue4",
+                          "darkturquoise","green1","yellow4","yellow3",
+                          "darkorange4","brown","black")
+names(colorCodesPop)<-unique(pop)
 labelCol <- function(x) {
   if (is.leaf(x)) {
     ## fetch label
@@ -14,7 +29,7 @@ labelCol <- function(x) {
 
 ### hierarchical clustering
 
-subset <- sample(2504,500)
+subset <- sample(2504,100)
 subset <- -related
 subset <- rep(T,2504)
 jm <- as.matrix(jaccardMatrix)[subset,subset]
@@ -26,6 +41,38 @@ d <- dendrapply(as.dendrogram(hc, hang=max(jm)*.1), labelCol)
 plot(d, main="Hierarchical Clustering across superpopulations", ylim=c(1-max(jm)*1.25,1))
 
 jm[jm>.005]<-.005
-heatmap.2(1-jm, dendrogram="none", trace="none",col=bluered)
+dev.off()
+heatmap.2(jm, Rowv=d, Colv=d, dendrogram="none", trace="none",
+          labRow="",labCol="",key=FALSE, RowSideColors=colorCodes[rownames(jm)], ColSideColors=colorCodesPop[colnames(jm)],
+#           lwid = c(1,10),lhei = c(.01,5), margins = c(5,10))
+)
+legend(.1,.5, names(colorCodes), inset=c(-0.01,0),  lty=1, lwd=10, col=colorCodes, cex = 0.75)
+legend("topright", names(colorCodesPop[1:13]),  lty=1, lwd=10, col=colorCodesPop[1:13], cex = 0.5, horiz = T,  inset=c(0,.05))
+legend("topright", names(colorCodesPop[14:26]),  lty=1, lwd=10, col=colorCodesPop[14:26], cex = 0.5, horiz = T,  inset=c(0,.10))
+
+plotHeatmap <-  function(x, subset=NA, title="GSM"){
+    if(is.na(subset)){
+        subset <- rep(T,nrow(x))
+    }
+    simMat <- as.matrix(x)[subset,subset]
+    diag(simMat)<-0
+    rownames(simMat) <- group[subset]
+    colnames(simMat) <- pop[subset]
+    hc <- hclust(as.dist(1-simMat),method="average")
+    d <- dendrapply(as.dendrogram(hc, hang=max(simMat)*.1), labelCol)
+    plot(d, main="Hierarchical Clustering across superpopulations", ylim=c(1-max(simMat)*1.25,1))
+    
+#     simMat[simMat>.005]<-.005
+    simMat[simMat>quantile(simMat,.99)]<-quantile(simMat,.99)
+    heatmap.2(simMat, Rowv=d, Colv=d, dendrogram="none", trace="none", main=title, col="bluered",
+              labRow="",labCol="",key=FALSE, RowSideColors=colorCodes[rownames(simMat)], ColSideColors=colorCodesPop[colnames(simMat)],
+              #           lwid = c(1,10),lhei = c(.01,5), margins = c(5,10))
+    )
+    legend("left", names(colorCodes), inset=c(0.1,0),  lty=1, lwd=10, col=colorCodes, cex = 0.75)
+    legend("topright", names(colorCodesPop[1:13]),  lty=1, lwd=10, col=colorCodesPop[1:13], cex = 0.5, horiz = T,  inset=c(0,.05))
+    legend("topright", names(colorCodesPop[14:26]),  lty=1, lwd=10, col=colorCodesPop[14:26], cex = 0.5, horiz = T,  inset=c(0,.10))
+}
+plotHeatmap(jaccardMatrix,title="Rare jaccard GSM")
+plotHeatmap(varcovMatrix,title="Varcov GSM")
 # for removing possible related individuals
 related <- which(jm>.1,arr.ind=T)[,1]
