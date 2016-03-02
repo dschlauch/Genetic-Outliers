f09982d5726f92856c022642438e23edb9be5a3f
Dan Schlauch <dschlauch@gmail.com>
2016-03-02 14:07:12 -0500

    refactoring, plotting, saving results mid-run

--- crypticness.R
+++ crypticness.R
@@ -1,7 +1,8 @@
 library(ggplot2)
 
 
-genotypeFile <- "./data/1000GP_Phase3_chr10.hap.gz"
+genotypeFile <- "./data/combinedFiltered1000.gz"
+numberOfLines <- 5000
 minVariants <- 10
 args<-commandArgs(TRUE)
 if(length(args)!=0){
@@ -29,10 +30,6 @@ hap.pop <- rep(pop,each=2)
 hap.sampleIDs <- rep(as.character(sample[,1]),each=2)
 
 source('~/1000GP/s_matrix_functions.R')
-# source('~/1000GP/all_samples_HCL.R')
-# calculateSMatrix(c("CEU"), numberOfLines=695, minVariants=20)
-# calculateSMatrix(c("CEU","CHB"), numberOfLines=10695, minVariants=20)
-# sapply(unique(pop),calculateSMatrix, numberOfLines=10695, minVariants=20)
 
 
 library(foreach)
@@ -47,23 +44,31 @@ if(!is.na(num_cores)){
 }
 
 res <- foreach(pop_i=unique(pop),.packages=c("ggplot2")) %dopar% {
-    calculateSMatrix(pop_i, filename=genotypeFile, numberOfLines=numberOfLines, minVariants=minVariants)
+    result <- calculateSMatrix(pop_i, filename=genotypeFile, numberOfLines=numberOfLines, minVariants=minVariants)
+    saveRDS(result,paste0("./plots/s_distributions/plotdata/",pop_i, "_data.rds"))
+    plotFromGSM(pop_i, result$s_matrix_dip, result$var_s_dip, result$weightsMean, sampleIDs[pop%in%pop_i], "diploid")
+}
+sapply(unique(pop),function(pop_i)){
+    result <- readRDS(paste0("./plots/s_distributions/plotdata/",pop_i, "_data.rds"))
+    plotFromGSM(pop_i, result$s_matrix_dip, result$var_s_dip, result$weightsMean, sampleIDs[pop%in%pop_i], "diploid")
+    s_vector <- result$s_matrix_dip[row(result$s_matrix_dip)>col(result$s_matrix_dip)]
+    binom.test(sum(s_vector>mean(s_vector)), length(s_vector), alternative="less")
 }
 
-res <- calculateSMatrix(unique(pop), filename=genotypeFile, numberOfLines=numberOfLines, minVariants=minVariants)
-res <- calculateSMatrix("CEU", filename=genotypeFile, numberOfLines=numberOfLines, minVariants=minVariants)
+# res <- calculateSMatrix(unique(pop), filename=genotypeFile, numberOfLines=numberOfLines, minVariants=minVariants)
+# res <- calculateSMatrix("CEU", filename=genotypeFile, numberOfLines=numberOfLines, minVariants=minVariants)
 
-res <- readRDS('~/1000GP/plots/s_distributions/plotdata/allSamples_sij_80695.rds')
-allSamplesGSM <- res[['s_i_j']]
-varcovMat <- res[['varcovMat']]
-
-pdf('~/1000GP/plots/s_matrix.pdf', width=9, height=9)
-plotHeatmap(allSamplesGSM, title="s GSM")
-dev.off()
-
-pdf('~/1000GP/plots/varcov_matrix.pdf', width=9, height=9)
-plotHeatmap(varcovMat,title="varcov GSM")
-dev.off()
+# res <- readRDS('~/1000GP/plots/s_distributions/plotdata/allSamples_sij_80695.rds')
+# allSamplesGSM <- res[['s_i_j']]
+# varcovMat <- res[['varcovMat']]
+# 
+# pdf('~/1000GP/plots/s_matrix.pdf', width=9, height=9)
+# plotHeatmap(allSamplesGSM, title="s GSM")
+# dev.off()
+# 
+# pdf('~/1000GP/plots/varcov_matrix.pdf', width=9, height=9)
+# plotHeatmap(varcovMat,title="varcov GSM")
+# dev.off()
 
 print(Sys.time()-strt)
 if(!is.na(num_cores)){
--- s_matrix_functions.R
+++ s_matrix_functions.R
@@ -1,6 +1,6 @@
 calculateSMatrix <- function(subpop="CEU", filename="./data/combinedFiltered1000.gz", numberOfLines=40695, minVariants=5, alpha=.01){
+    require(readr)
     print(subpop)
-    filename <- "./data/combinedFiltered1000.gz"
     
     con <- file(filename, "rt")
     system.time(genotypes <- apply(do.call(cbind, strsplit(readLines(con, numberOfLines)," ")), 1,as.numeric)[,hap.pop%in%subpop])
@@ -25,69 +25,45 @@ calculateSMatrix <- function(subpop="CEU", filename="./data/combinedFiltered1000
     totalPairs <- choose(sumFilteredVariants,2)
     weights <- totalPossiblePairs/totalPairs
     p <- 1/weights
-    var_s <- sum((1-p)/p)/(numFilteredVariants^2)
-    print("variance of s")
-    print(var_s)
+    var_s_hap <- sum((1-p)/p)/(numFilteredVariants^2)
+    print("variance of s (haploid)")
+    print(var_s_hap)
     
     # Calculate expected values conditional on kinship
     meanOfWeights <- mean(weights)
     kinships <- seq(0,.25,.001)
     kinshipExpectation <- 1+kinships*(meanOfWeights-1)
         
-    ggplot(data.frame(kinships, kinshipExpectation), aes(x=kinships, y=kinshipExpectation))+ geom_point()
-    #     num_comparisons <- numSamples*(numSamples-1)/2
-    #     bonferroni_cutoff <- qnorm((1-alpha/(2*num_comparisons)), sd=sqrt(var_s)) + 1
+    s_matrix_numerator <- t(genotypes*weights)%*%genotypes
+    s_matrix_denominator <- numFilteredVariants
+    s_matrix_hap <- s_matrix_numerator/s_matrix_denominator
     
-    s.i.j.numerator <- t(genotypes*weights)%*%genotypes
-    s.i.j.denominator <- numFilteredVariants
-    s.i.j <- s.i.j.numerator/s.i.j.denominator
+    print(mean(s_matrix_hap[row(s_matrix_hap)!=col(s_matrix_hap)]))
+    print(median(s_matrix_hap[row(s_matrix_hap)!=col(s_matrix_hap)]))
     
-    print(mean(s.i.j[row(s.i.j)!=col(s.i.j)]))
-    print(median(s.i.j[row(s.i.j)!=col(s.i.j)]))
-    
-    estimatedKinship <- (s.i.j-1)/(meanOfWeights-1)
-    plotFromGSM(subpop, s.i.j, var_s, estimatedKinship, hap.sampleIDs.subset, "haploid")
+    estimatedKinship <- (s_matrix_hap-1)/(meanOfWeights-1)
     
     # Collapse to diploid
-    s.i.j.dip <- (s.i.j[c(T,F),c(T,F)] + s.i.j[c(F,T),c(T,F)] +s.i.j[c(T,F),c(F,T)] + s.i.j[c(F,T),c(F,T)])/4
+    s_matrix_dip <- (s_matrix_hap[c(T,F),c(T,F)] + s_matrix_hap[c(F,T),c(T,F)] +s_matrix_hap[c(T,F),c(F,T)] + s_matrix_hap[c(F,T),c(F,T)])/4
     # very lazy variance estimate...
-    varS <- var_s/4
+    var_s_dip <- var_s_hap/4
     
-    estimatedKinshipDip <- (s.i.j.dip-1)/(meanOfWeights-1)
-    plotFromGSM(subpop,s.i.j.dip, varS, estimatedKinshipDip, sampleIDs.subset, "diploid")
-    
-    
-    
-    saveRDS(s.i.j, paste0("./plots/s_distributions/plotdata/",paste0(subpop,collapse="_"),"_sij_hap.rds"))
-    saveRDS(s.i.j.dip, paste0("./plots/s_distributions/plotdata/",paste0(subpop,collapse="_"),"_sij_dip.rds"))
-    saveRDS(varcovMat, paste0("./plots/s_distributions/plotdata/",paste0(subpop,collapse="_"),"_varcovMat.rds"))
-    list(s_i_j = s.i.j.dip, varcovMat=varcovMat)
+
+    list(s_matrix_dip=s_matrix_dip, s_matrix_hap=s_matrix_hap, weightsMean=meanOfWeights, var_s_dip=var_s_dip, var_s_hap=var_s_hap, varcovMat=varcovMat)
 }
-plotFromGSM <- function(subpop, gsm, varS, kinship, sample_IDs, plotname="", alpha=.01){
+plotFromGSM <- function(subpop, gsm, var_s, weightsMean, sample_IDs, plotname="", alpha=.01){
     print(mean(gsm[row(gsm)!=col(gsm)]))
     print(median(gsm[row(gsm)!=col(gsm)]))
     num_comparisons_dip <- choose(ncol(gsm),2)
     
-    bonferroni_cutoff_dip <- qnorm((1-alpha)^(1/num_comparisons_dip), sd=sqrt(varS)) + 1
+    bonferroni_cutoff_dip <- qnorm((1-alpha)^(1/num_comparisons_dip), sd=sqrt(var_s)) + 1
     
     topValuesDip <- sort(gsm[row(gsm)>col(gsm)], decreasing=T)
-    topValuesKinship <- sort(kinship[row(kinship)>col(kinship)], decreasing=T)
+    topValuesKinship <- (topValuesDip-1)/(weightsMean-1)
     
-    topDipIndices <- sapply(1:10, function(x){
-        which(gsm==topValuesDip[x], arr.ind=T)[1,]
-    })
-    
-    # This needed to prevent errors when there are no significant results
-    if (sum(topValuesDip[1:10]>bonferroni_cutoff_dip)>0){
-        topDipIndices <- topDipIndices[,topValuesDip[1:10]>bonferroni_cutoff_dip]
-        mappedTopDipHits <- matrix(sample_IDs[topDipIndices],nrow=2)
-        topHitsNamesDip <- apply(mappedTopDipHits,2,paste0,collapse="_")
-        topValuesDip <- topValuesDip[topValuesDip>bonferroni_cutoff_dip]
-        topValuesDip <- head(topValuesDip,10)
-    } else {
-        topHitsNamesDip <- NA
-        topValuesDip <- 1
-    }
+    # Display only those that are above the cutoff and among the top 5
+    label_cutoff <- max(bonferroni_cutoff_dip, topValuesDip[5])
+ 
     pairs <- outer(sample_IDs, sample_IDs, paste)
     plotData <- data.frame(values=gsm[row(gsm)>col(gsm)], pairs=paste0("  ",pairs[row(pairs)>col(pairs)]))
     minDip <- min(plotData$values)
@@ -95,19 +71,19 @@ plotFromGSM <- function(subpop, gsm, varS, kinship, sample_IDs, plotname="", alp
     dipPlot <- ggplot(plotData, aes(values)) + 
         geom_histogram(color="blue",binwidth=.01,fill=I("blue")) + 
         ggtitle(paste0(subpop,collapse="_"))  + xlab("Similarity score") + 
-        scale_x_continuous(limits=c(0, maxDip+1)) +
+        scale_x_continuous(expand=c(.2,0))+#limits=c(minDip, maxDip+1)) +
         theme_bw() +
         theme(plot.title = element_text(size=40), axis.title.x = element_text(size = 10), axis.title.y = element_blank(), axis.text.y=element_blank()) + 
         
         geom_vline(xintercept = bonferroni_cutoff_dip, color="red", linetype="dotted") + 
         geom_vline(data=subset(plotData, (values == maxDip & values>bonferroni_cutoff_dip)),aes(xintercept = values), color="blue", linetype="dotted") + 
-        geom_vline(xintercept = median(gsm[row(gsm)!=col(gsm)]), color="red", linetype=1) + 
+        geom_vline(xintercept = median(gsm[row(gsm)!=col(gsm)]), color="black", linetype=1) + 
         
-        geom_text(data=subset(plotData, values > bonferroni_cutoff_dip), aes(values,label=pairs), y=0, angle = 80, hjust=0, size=3) +
-        geom_text(data=subset(plotData, (values == maxDip & values>bonferroni_cutoff_dip)), x=maxDip, y=Inf, label=paste0("hat(phi)==", round(topValuesKinship[1],3),"  "),parse = TRUE, color="blue", angle = 0, size = 3, vjust = 1, hjust = 0) +
+        geom_text(data=subset(plotData, values > label_cutoff), aes(values,label=pairs), y=0, angle = 80, hjust=0, size=3) +
+        geom_text(data=subset(plotData, (values == maxDip & values>bonferroni_cutoff_dip)), x=maxDip, y=Inf, label=paste0("hat(phi)==", round(topValuesKinship[1],4),"  "),parse = TRUE, color="blue", angle = 0, size = 3, vjust = 1, hjust = 0) +
         
         annotate("text", x=bonferroni_cutoff_dip, y=Inf, label=paste0("alpha==",format(alpha/num_comparisons_dip, digits=1)),parse = TRUE, color="red", angle = 0, size = 3, vjust = 1, hjust = 1) +
-        annotate("text", x=median(gsm[row(gsm)!=col(gsm)]), y=Inf, label=paste0("m=",round(median(gsm[row(gsm)!=col(gsm)]),3)," "), color="red", angle = 0, size = 3, vjust = 1, hjust = 0) 
+        annotate("text", x=median(gsm[row(gsm)!=col(gsm)]), y=Inf, label=paste0("m=",round(median(gsm[row(gsm)!=col(gsm)]),3)," "), color="black", angle = 0, size = 3, vjust = 1, hjust = 1) 
     
     
     
